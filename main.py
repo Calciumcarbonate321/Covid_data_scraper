6cb592549ebaf90cb8d68b912bee5a5c770378bc
Visnu <visnuram2004@gmail.com>
2020-12-19 18:59:12 +0530

    Webscraping done, GUI left

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+#IMPORTING ALL NECESSARY FILES
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import dask.dataframe as dd
 import warnings
+import csv
 
+user_country=input("Enter your country name: ")
+#SCRAPING THE SITE
 page=requests.get("https://www.worldometers.info/coronavirus")
 soup=BeautifulSoup(page.content, 'lxml')
 table=soup.find('table', attrs={'id':'main_table_countries_today'})
@@ -26,5 +30,39 @@ dt = pd.DataFrame(data)
 dt = pd.DataFrame(data[1:], columns=data[0][:12])
 df = dd.from_pandas(dt,npartitions=1)
 
-df.to_csv('./Extracted_data/data-*.csv')
-#simply
+df.to_csv('extracted_data*.csv')
+
+data_csv=list(csv.reader('extracted_data0.csv'))
+data_file='extracted_data0.csv'
+with open(data_file,'r') as covid_database:
+    csvfile=csv.reader(covid_database)
+    for i in csvfile:
+        if user_country in i:
+            print("\n")
+            print("Total cases reported: ",i[3])
+            if i[4]=='':
+                print("\n")
+                print("Total new cases reported today: 0 ")
+            else:
+                print("\n")
+                print("Total new cases reported today: ",i[4])
+            print("\n")
+            print("Total deaths reported: ",i[5])
+            if i[6]=='':
+                print("\n")
+                print("Total deaths reported today: 0")
+            else:
+                print("\n")
+                print("Total deaths reported today: ",i[6])
+            print("\n")
+            print("Total number of people who recovered: ",i[7])
+            if i[8]=='':
+                print("\n")
+                print("Total number of people who recovered today: 0")
+            else:
+                print("\n")
+                print("Total number of people who recovered today: ",i[8])
+            print("\n")
+            print("Total number of active cases: ",i[9])
+            print("\n")
+            print("Total number of serious cases: ",i[10])
